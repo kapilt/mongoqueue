56efaa1c605a668937f356d3cd32b3820dabd866
j.c.sackett <j.c.sackett@gmail.com>
2013-02-04 20:44:41 +0000

    Increment version number.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='mongoqueue',
-      version="0.5.2",
+      version="0.6.0",
       classifiers=[
           'Intended Audience :: Developers',
           'Programming Language :: Python',
